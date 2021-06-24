ebd6b59732f46b65e5acc304e7a648eece86cc75
Nic Appleby <nic.appleby@balfourgroup.com>
2021-06-24 14:51:27 +0200

    fix path

--- src/Installer.php
+++ src/Installer.php
@@ -6,11 +6,6 @@ class Installer
 {
     public static function postInstall()
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            system('cmd /c vendor\wickedreports\phpcs-pre-commit-hook\src\setup.bat');
-        }
-        else {
-            system('sh vendor/wickedreports/phpcs-pre-commit-hook/src/setup.sh');
-        }
+        system('sh src/vendor/balfour-group/phpcs-pre-commit-hook/src/setup.sh');
     }
 }
