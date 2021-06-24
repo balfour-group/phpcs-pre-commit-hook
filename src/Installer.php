1f21b7014344a21216a446095385cd04ac996c7e
Nic Appleby <nic.appleby@balfourgroup.com>
2021-06-24 14:53:12 +0200

    fix path

--- src/Installer.php
+++ src/Installer.php
@@ -6,6 +6,6 @@ class Installer
 {
     public static function postInstall()
     {
-        system('sh src/vendor/balfour-group/phpcs-pre-commit-hook/src/setup.sh');
+        system('sh vendor/balfour-group/phpcs-pre-commit-hook/src/setup.sh');
     }
 }
