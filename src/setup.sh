8271c7c672f2ae94e8facb2c98f83040a124a5c9
Nic Appleby <nic.appleby@balfourgroup.com>
2021-06-24 16:10:42 +0200

    Make it work for packages

--- src/setup.sh
+++ src/setup.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-if [ -e ../.git/hooks/pre-commit ];
+if [ -d .git ];
+then
+    DIR_PREFIX=
+else
+    DIR_PREFIX=../
+fi
+
+if [ -e "$DIR_PREFIX.git/hooks/pre-commit" ];
 then
     PRE_COMMIT_EXISTS=1
 else
     PRE_COMMIT_EXISTS=0
 fi
 
-if [ ! -d ../.git/hooks ]; then
-  mkdir -p ../.git/hooks
+if [ ! -d "$DIR_PREFIX.git/hooks" ]; then
+  mkdir -p "$DIR_PREFIX.git/hooks"
 fi
 
-cp vendor/balfour-group/phpcs-pre-commit-hook/src/pre-commit ../.git/hooks/pre-commit
-chmod +x ../.git/hooks/pre-commit
+cp vendor/balfour-group/phpcs-pre-commit-hook/src/pre-commit "$DIR_PREFIX.git/hooks/pre-commit"
+chmod +x "$DIR_PREFIX.git/hooks/pre-commit"
 
 if [ "$PRE_COMMIT_EXISTS" = 0 ];
 then
